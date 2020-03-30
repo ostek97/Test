f81f7d9368e7e5c7a835870479c7d71b957eda74
Kondzio817 <61422223+Kondzio817@users.noreply.github.com>
2020-03-30 20:58:45 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+	 System.out.println("         <3          <3");
+	 System.out.println("       <3  <3      <3  <3");
+	 System.out.println("    <3       <3  <3      <3");
+	 System.out.println("  <3           <3          <3");
+	 System.out.println("<3  Ostek KOCHA SIĘ w Marcie <3");
+	 System.out.println("  <3                       <3");
+	 System.out.println("    <3                   <3");
+	 System.out.println("      <3              <3");
+	 System.out.println("        <3          <3");
+	 System.out.println("          <3      <3");
+	 System.out.println("            <3  <3");
+	 System.out.println("              <3");
 	// write your code here
     }
 }
